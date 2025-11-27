30add84a65f7a51d1d8033806cb546db1ef5eed2
Edward <editaliano123@gmail.com>
2025-11-27 15:30:06 -0300

    feature: integrating with elastic apm

--- app.py
+++ app.py
@@ -4,6 +4,11 @@ from services.logger import logger
 
 app = FastAPI(title="FastAPI on Lambda")
 
+from elasticapm.contrib.starlette import make_apm_client, ElasticAPM
+
+apm = make_apm_client()
+app.add_middleware(ElasticAPM, client=apm)
+
 @app.get("/health")
 def health_check():
     logger.debug("Health check endpoint called.")
@@ -17,24 +22,20 @@ def create_item():
     except Exception as e:
         return {"error": str(e)}
 
+
 @app.get("/function_with_raise")
 def function_with_raise():
     try:
         raise ValueError("An intentional error occurred.")
     except Exception as e:
         logger.error(f"Error inaa all_items: {e}", exc_info=True)
-        raise HTTPException(
-            status_code=500,
-            detail="Internal server error"
-        )
-    
+        raise HTTPException(status_code=500, detail="Internal server error")
+
+
 @app.get("/function_with_error")
 def function_with_error():
     try:
         call_function_not_implemented()
     except Exception as e:
         logger.error(f"Error inaa all_items: {e}", exc_info=True)
-        raise HTTPException(
-            status_code=500,
-            detail="Internal server error"
-        )
+        raise HTTPException(status_code=500, detail="Internal server error")
